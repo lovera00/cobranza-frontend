f66464f054ac3e195d2f6f57ba80438a02e6e953
Lovera00 <mathiasalejandroloverasilva@gmail.com>
2023-02-18 18:42:31 -0300

    listar Tipos gestiones added

--- src/app/pages/tipogestiones/tipogestiones.component.spec.ts
+++ src/app/pages/tipogestiones/tipogestiones.component.spec.ts
@@ -1,23 +1,103 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { TipogestionesComponent } from './tipogestiones.component';
+import { Estado, Tipogestiones } from './tipoGestiones.interface';
+import { TipogestionesService } from './tipogestiones.service';
 
 describe('TipogestionesComponent', () => {
   let component: TipogestionesComponent;
+  let service : TipogestionesService;
   let fixture: ComponentFixture<TipogestionesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TipogestionesComponent ]
+      declarations: [ TipogestionesComponent ],
+      imports: [HttpClientModule , AppRoutingModule , ReactiveFormsModule],
+      providers: [TipogestionesService]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(TipogestionesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    service = TestBed.inject(TipogestionesService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should decrease the value of "desde" by 5 when calling previousPage', () => {
+    component.desde = 5;
+    component.previousPage();
+    expect(component.desde).toEqual(0);
+  })
+
+  it('should not decrease the value of "desde" if "desde" is already 0', () => {
+    component.desde = 0;
+    component.previousPage();
+    expect(component.desde).toEqual(0);
+  });
+
+  it('should add the value of "desde" by 5 when calling nextPage', () => {
+    component.desde = 0;
+    component.nextPage();
+    expect(component.desde).toEqual(5);
+  })
+
+  it('should call "listarTipoGestiones" when "nextPage" is called', () => {
+    spyOn(component, 'listarTipoGestiones');
+    component.nextPage();
+    expect(component.listarTipoGestiones).toHaveBeenCalled();
+  });
+
+  it('should call "listarTipoGestiones" when "previousPage" is called', () => {
+    component.desde = 5;
+    spyOn(component, 'listarTipoGestiones');
+    component.previousPage();
+    expect(component.listarTipoGestiones).toHaveBeenCalled();
+  })
+
+  it('sholud not call "listarTipoGestiones" where previus page is called and desde equal 0 or less',()=>{
+    component.desde = 0;
+    spyOn(component, 'listarTipoGestiones');
+    component.previousPage();
+    expect(component.listarTipoGestiones).not.toHaveBeenCalled();
+  })
+  it('should call "listarTipoGestiones" in the start up', () => {
+    spyOn(component, 'listarTipoGestiones');
+    component.ngOnInit();
+    expect(component.listarTipoGestiones).toHaveBeenCalled();
+  })
+
+  it('should set tipoGestion and loading variables correctly when getAll() method of TipoGestionesService returns data', () => {
+    const tipoGestiones:Tipogestiones[] = [
+      {
+        id:1,
+        description:'Gestion 1',
+        status:Estado.Activo
+      },
+      {
+        id:2,
+        description:'Gestion 2',
+        status:Estado.Activo
+      }
+    ];
+
+    spyOn(service, 'getAll').and.returnValue(
+      new Observable<Tipogestiones[]>(subscriber => {
+        subscriber.next(tipoGestiones);
+      })
+    );
+    component.listarTipoGestiones();
+    expect(component.loading).toBeFalse();
+    expect(component.tipoGestion).toEqual(tipoGestiones);
+  })
+
+  
 });
--- src/app/pages/tipogestiones/tipogestiones.component.ts
+++ src/app/pages/tipogestiones/tipogestiones.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Tipogestiones } from './tipoGestiones.interface';
+import { TipogestionesService } from './tipogestiones.service';
 
 @Component({
   selector: 'app-tipogestiones',
   templateUrl: './tipogestiones.component.html',
   styleUrls: ['./tipogestiones.component.css']
 })
-export class TipogestionesComponent {
+export class TipogestionesComponent implements OnInit {
+  constructor(
+    private readonly tipoGestionesService:TipogestionesService,
+    private formBuilder: FormBuilder
+  ) { }
+
+  desde: number = 0;
+  hasta: number = 5;
+  tipoGestion: Tipogestiones[] = [];
+  addForm: FormGroup = this.formBuilder.group({});
+  updateForm: FormGroup = this.formBuilder.group({});
+  loading: boolean = false;
+  mostrarVistaCrear = true;
+  mostrarVistaActualizar = false;
+  ngOnInit() {
+    this.loading = true;
+    this.listarTipoGestiones();
+    this.addForm = this.formBuilder.group({
+      descripcion: ['', Validators.required],
+      status: ['',Validators.required]
+    });
+    this.updateForm = this.formBuilder.group({
+      id:['',Validators.required],
+      descripcion: ['', Validators.required],
+      status: ['',Validators.required]
+    });
+  }
+  previousPage() {
+    if (this.desde <= 0) {
+      this.desde = 0;
+    } else {
+      this.desde -= 5;
+      this.listarTipoGestiones();
+    }
+  }
+  nextPage() {
+    this.desde += 5;
+    this.listarTipoGestiones();
+  }
+  listarTipoGestiones() {
+    this.tipoGestionesService.getAll(this.desde, this.hasta).subscribe(
+      (data:Tipogestiones[])=>{
+        this.tipoGestion = data;
+        this.loading = false;
+      }
+    );
+  }
 
 }
